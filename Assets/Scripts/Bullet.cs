32c346a87c15df53795701a711da6cc37511557e
Kareem <kmeawad@uwo.ca>
2024-02-24 19:16:17 -0500

    Add bullet logic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,20 +7,34 @@ public class Bullet : MonoBehaviour
     public float speed; // variable for the speed
     public float damage; // variable for how much damage the bullet deals
 
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+        //get the camera height and width
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (pos.x > camWidth || pos.x < -camWidth || pos.y > camHeight || pos.y < -camHeight)
+        {
+            Destroy(gameObject, 0.1f);
+        }
+
     }
 
     //call function when collider is triggered
-    public void OnTriggerEnter2D(Collider2D collision)
+    public virtual void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject, 0.1f);
 
